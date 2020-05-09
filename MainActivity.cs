68691d24049a9eabfdaedbb4b84403fc14783dab
Shazad Ishaque <shazad_ishaque@yahoo.co.uk>
2020-05-09 13:26:29 +0100

    Calculate bill

--- MainActivity.cs
+++ MainActivity.cs
@@ -12,12 +12,24 @@ namespace TipCalculator
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button calculateButton;
+        EditText inputBill;
+        TextView outputTip;
+        TextView outputTotal;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
+            inputBill.TextChanged += OnBillKeyPress;
 
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
@@ -46,6 +58,35 @@ namespace TipCalculator
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-	}
+
+        private void OnBillKeyPress(object sender, EventArgs e)
+        {
+            CalculateBill();
+        }
+
+        private void OnCalculateClick(object sender, EventArgs e)
+        {
+            CalculateBill();
+        }
+
+        private void CalculateBill()
+        {
+            outputTip.Text = "";
+            outputTotal.Text = "";
+
+            string text = inputBill.Text;
+            double bill = 0;
+
+            if (double.TryParse(text, out bill))
+            {
+                double tip = bill * 0.15;
+                double total = bill + tip;
+
+                outputTip.Text = tip.ToString("#,0.00");
+                outputTotal.Text = total.ToString("#,0.00");
+            }
+        }
+
+    }
 }
 
